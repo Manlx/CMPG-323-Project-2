0a1071aa91d1b8970c80d6388af85b2e566af0ec
Manlx <74509303+Manlx@users.noreply.github.com>
2022-08-30 16:46:06 +0200

    Cleared project started from scratch. Added All Packages.

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,8 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
-namespace Project2Api
+namespace Project2API
 {
     public class Startup
     {
